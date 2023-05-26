26188c405d180fddec7323912595d2d629197c8d
‪Elsayed Mustafa‬‏ <110793510+El-Sayed-Mustafa@users.noreply.github.com>
2023-05-26 15:45:57 +0300

    best seller item p2

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../../../../../core/utils/style.dart';
 import 'custom_app_bar.dart';
 import 'custom_list_view.dart';
-import 'custom_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,7 +10,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
@@ -24,6 +23,7 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.textStyle18,
           ),
+          SizedBox(height: 20),
           BestSellerListViewItem(),
         ],
       ),
@@ -47,9 +47,24 @@ class BestSellerListViewItem extends StatelessWidget {
                   borderRadius: BorderRadius.circular(10),
                   color: Colors.red,
                   image: const DecorationImage(
-                      fit: BoxFit.fill, image: AssetImage(AssetsData.testImage))),
+                      fit: BoxFit.fill,
+                      image: AssetImage(AssetsData.testImage))),
             ),
           ),
+          const SizedBox(width: 30),
+          Column(
+            children:  [
+              SizedBox(
+                width: MediaQuery.of(context).size.width*.5,
+                child: const Text(
+                  'Harry Potter and the Goblet of fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20,
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
