63b6c2344d1d6dcf15304192bdbf03e87e325841
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-06-22 18:51:26 +0300

    implement fetchBestSellerBooks

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -4,13 +4,26 @@ import 'package:e_bookly/core/errors/failure.dart';
 
 import 'package:dartz/dartz.dart';
 
+import '../../../../core/utils/API_service.dart';
 import 'home_repo.dart';
 
 class HomeRepoImp implements HomeRepo {
+  late final ApiService apiService;
   @override
-  Future<Either<Failure, List<BookModels>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModels>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming');
+
+      List<BookModels> books = [];
+      for (var item in data['items']) {
+        books.add(BookModels.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
