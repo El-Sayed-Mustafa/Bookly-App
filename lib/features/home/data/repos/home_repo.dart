786cc7394def4d3f3261eea63959f59d6f4ab75f
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-06-22 15:32:29 +0300

    create HomeRepo and failure file

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_bookly/features/home/data/models/book_models/book_models.dart';
+
+import '../../../../core/errors/failure.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModels>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModels>>> fetchFeaturedBooks();
+}
