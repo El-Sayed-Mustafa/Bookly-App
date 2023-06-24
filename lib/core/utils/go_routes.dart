364537c5600672779b096959e49295def99c50d3
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-06-24 10:25:02 +0300

    fetchSimilarBooks done

--- lib/core/utils/go_routes.dart
+++ lib/core/utils/go_routes.dart
@@ -1,8 +1,13 @@
+import 'package:e_bookly/core/utils/service_locator.dart';
+import 'package:e_bookly/features/home/data/repos/home_repo_impl.dart';
 import 'package:e_bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:e_bookly/features/home/presentation/views/home_view.dart';
 import 'package:e_bookly/features/search/presentation/views/search_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/home/data/models/book_models/book_models.dart';
+import '../../features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart';
 import '../../features/splash/presentaion/views/splash_view.dart';
 
 abstract class AppRoute {
@@ -20,7 +25,12 @@ abstract class AppRoute {
     ),
     GoRoute(
       path: kBookDetailsView,
-      builder: (context, state) => const BookDetailsView(),
+      builder: (context, state) => BlocProvider(
+        create: (context) => SimilarBooksCubit(getIt.get<HomeRepoImp>()),
+        child: BookDetailsView(
+          book: state.extra as BookModels,
+        ),
+      ),
     ),
     GoRoute(
       path: kSearchView,
