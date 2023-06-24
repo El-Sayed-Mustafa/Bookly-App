c6f3e7e9788cb59b8f25c9277444a33377b2647f
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-06-24 11:59:14 +0300

    view books in search screen

--- lib/features/home/presentation/views/widgets/similer_books_listview.dart
+++ lib/features/home/presentation/views/widgets/similer_books_listview.dart
@@ -3,7 +3,9 @@ import 'package:e_bookly/core/utils/widgets/custom_loading_indicator.dart';
 import 'package:e_bookly/features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../../core/utils/go_routes.dart';
 import 'custom_book_image.dart';
 
 class SimilarBooksListView extends StatelessWidget {
@@ -20,12 +22,18 @@ class SimilarBooksListView extends StatelessWidget {
                 itemCount: state.books.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: CustomBookImage(
-                      imageUrl: state
-                              .books[index].volumeInfo.imageLinks?.thumbnail ??
-                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKSPVqHle9tBvH8OgnpmF9nAAefDmGTWvk3wGfbInA0x9Uql3v0uZIxkguUYcZ6fbRcS0&usqp=CAU',
+                  return GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).push(AppRoute.kBookDetailsView,
+                          extra: state.books[index]);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: CustomBookImage(
+                        imageUrl: state.books[index].volumeInfo.imageLinks
+                                ?.thumbnail ??
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKSPVqHle9tBvH8OgnpmF9nAAefDmGTWvk3wGfbInA0x9Uql3v0uZIxkguUYcZ6fbRcS0&usqp=CAU',
+                      ),
                     ),
                   );
                 }),
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,8 @@
+import 'package:e_bookly/core/utils/widgets/custom_error_widget.dart';
+import 'package:e_bookly/core/utils/widgets/custom_loading_indicator.dart';
+import 'package:e_bookly/features/home/presentation/manger/Featured_books_cubit/feature_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../core/utils/style.dart';
 import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
@@ -40,15 +44,26 @@ class SearchResultListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10.0),
-          // child: BookListViewItem(),
-          child: Text("temp"),
-        );
+    return BlocBuilder<FeatureBooksCubit, FeatureBooksState>(
+      builder: (context, state) {
+        if (state is FeatureBooksSuccess) {
+          return ListView.builder(
+            padding: EdgeInsets.zero,
+            itemCount: state.books.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: BookListViewItem(
+                  model: state.books[index],
+                ),
+              );
+            },
+          );
+        } else if (state is FeatureBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
       },
     );
   }
